b427837644f52adffd3b56d5f75c4abc89ec6a33
Nikeeth <nikeeth.ramanathan@gmail.com>
2020-12-02 11:39:16 +1100

    ec2 setup initialise db

--- projects/stf_timescale_db/ec2-setup.sh
+++ projects/stf_timescale_db/ec2-setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -ex
 
-# install docker
+# install things
 sudo yum update -y
 sudo amazon-linux-extras install docker
 sudo yum install docker -y
+sudo yum install python3 -y
+
+# start docker
+sudo systemctl enable docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
@@ -12,6 +16,22 @@ sudo curl -L \
     "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
     -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# --- run as user ---
+
+# - clone repo
+# - start up timescaledb
+# - run ingestion scripts
+# - start up other containers
+
+sudo -u ec2-user bash <<'EOF'
+    cd /home/ec2-user
+    git clone https://github.com/nikeethr/data_engineering.git
+    cd data_engineering/projects/stf_timescale_db
+    docker-compose up -d stf_db
+EOF
+
+# ---
 
 # TODO:
 # - git
