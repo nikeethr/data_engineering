072dfc25dcc6f9be330a602c25f33b6d11594d24
Nikeeth <nikeeth.ramanathan@gmail.com>
2020-12-02 15:57:22 +1100

    bugs

--- projects/stf_timescale_db/get_sample_data_from_s3.sh
+++ projects/stf_timescale_db/get_sample_data_from_s3.sh
@@ -7,5 +7,6 @@ set -u
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 TEST_BUCKET=stf-prototype-sample-data
 TEST_DATA="${SCRIPT_DIR}/scripts/stf_ingest/sample_data"
+S3_TEST_DATA="s3://${TEST_BUCKET}/sample_data"
 
 aws s3 sync $S3_TEST_DATA $TEST_DATA
