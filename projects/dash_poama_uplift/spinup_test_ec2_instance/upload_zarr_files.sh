fd4cf6983d519dc7f92a6a2f3928cedb869076d9
Nikeeth <nikeeth.ramanathan@gmail.com>
2020-12-14 16:29:19 +1100

    update ec2 instance to have access to deploy lambda stack via SAM

--- projects/dash_poama_uplift/spinup_test_ec2_instance/upload_zarr_files.sh
+++ projects/dash_poama_uplift/spinup_test_ec2_instance/upload_zarr_files.sh
@@ -2,16 +2,16 @@
 
 set -eu
 
-script_dir=$(dirname $(readlink -f $0))
+. ./deploy_scripts.cfg
+
+script_dir="$(dirname $(readlink -f $0))/scripts"
 nc_to_zarr="${script_dir}/netcdf_to_zarr.py"
 default_nc_dir="${script_dir}/netcdf_in"
-default_test_bucket=fvt-test-zarr-nr
-region=ap-southeast-2
 zarr_out="${script_dir}/zarr_out"
 
 # read args if any or use defaults
 nc_in=${1:-$default_nc_dir}
-s3_bucket=${2:-$default_test_bucket}
+s3_bucket=${2:-$DATA_BUCKET}
 
 # convert to zarr
 python3 $nc_to_zarr $nc_in $zarr_out
@@ -20,7 +20,7 @@ python3 $nc_to_zarr $nc_in $zarr_out
 bucket_exists=$(aws s3 ls | grep -c "${s3_bucket}")
 
 if [ "${bucket_exists}" -eq "0" ]; then
-    aws s3 mb "s3://${s3_bucket}" --region $region
+    aws s3 mb "s3://${s3_bucket}" --region $REGION
 else
     echo "bucket: ${s3_bucket} already exists. skipping..."
 fi
