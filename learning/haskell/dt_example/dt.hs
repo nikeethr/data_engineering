62f71f2ad42274dc68762b860fcb09c38c014e22
Nikeeth <nikeeth.ramanathan@gmail.com>
2021-06-05 15:53:25 +1000

    minor changes to make function work - note fromJust should be replaced with fmap

--- learning/haskell/dt_example/dt.hs
+++ learning/haskell/dt_example/dt.hs
@@ -10,14 +10,19 @@ parseUTCTime x = fromJust (parseTimeM True defaultTimeLocale "%Y%m%dT%H%Z" (x ++
 
 checkTimeThreshold :: String -> String -> (Bool, M.HashMap String String)
 checkTimeThreshold x y
-    | (xUTC `hoursDiff` yUTC) < thresholdHours = (True, fromList [("1", "2"), ("3", "4")])
-    | otherwise = (False, M.empty)
+    | 0 <= h && h < thresholdHours = proceed
+    | h < 0 = wait
+    | otherwise = skip
     where
         xUTC = parseUTCTime x
         yUTC = parseUTCTime y
+        h = xUTC `diffHours` yUTC
+        proceed = (True, fromList [("action", "proceed")])
+        skip = (True, fromList [("action", "skip")])
+        wait = (False, M.empty)
         thresholdHours = 12
-        hoursDiff :: UTCTime -> UTCTime -> Float
-        hoursDiff x y = ((fromIntegral $ fst $ properFraction $ x `diffUTCTime` y) :: Float) / 3600
+        diffHours :: UTCTime -> UTCTime -> Float
+        diffHours x y = ((fromIntegral $ fst $ properFraction $ x `diffUTCTime` y) :: Float) / 3600
 
 -- utcX = parseUTCTime
 -- utcY = parseUTCTime
