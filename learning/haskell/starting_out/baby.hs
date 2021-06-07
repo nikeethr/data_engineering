9a3d39466a2b92ca0ba9ffff3349cbadf85e1656
Nikeeth <nikeeth.ramanathan@gmail.com>
2021-06-08 08:33:58 +1000

    more learning haskell

--- learning/haskell/starting_out/baby.hs
+++ learning/haskell/starting_out/baby.hs
@@ -102,3 +102,66 @@ decimalStep = [0.1,0.3..1]
 -- cycle: like python generators - creates infinite repeated pattern from a list
 tenOneTwoThrees = take 10 (cycle [1,2,3])
 threeLOLs = take 11 (cycle "LOL ")
+-- repeat: like cycling but only with 1 element
+repeatFive = take 10 (repeat 5)
+-- replicate is a simpler option for this sometimes
+replicateTen = replicate 3 10
+
+------------------------
+-- List comprehension --
+------------------------
+-- It's set theory, for complex functions applied on lists with a predicate
+-- S = [ 2*x | x <- N, x <= 10 ]
+xSquared = [ x*2 | x <- [1..10] ]
+-- adding predicate
+xSquaredLarge = [ x*2 | x <- [1..10], x*2 >= 12 ]
+-- more complex conditions
+remThree = [ x | x <- [50..100], x `mod` 7 == 3 ]
+-- conditional statement: take a list, find all odd values in the list, spit
+-- out BOOM! for values less than 10 or BANG! for those greater than or equal
+-- to 10: Also this is a function
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+-- multiple predicates: Union of predicates
+multiplePredicates = [ x | x <- [10..20], x /= 13, x /= 15, x /= 19 ]
+-- product all pair of elements in list
+productList = [ x*y | x <- [2,5,10], y <- [8,10,11] ]
+-- only greater than 50
+productListLarge = [ x*y | x <- [2,5,10], y <- [8,10,11], x*y > 50 ]
+-- strings?: adjective + noun
+nouns = ["artist","donkey","carrot"]
+adjectives = ["beautiful","lazy","vengeful"]
+phrases = [ y ++ " " ++ x | x <- nouns, y <- adjectives ]
+-- alternate version of length
+-- NOTE: _ is a placeholder for a unused variable (similar to other languages)
+length' xs = sum [1 | _ <- xs]
+-- strings are lists, so these operations apply to them
+-- example: remove non-uppercase
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
+-- nested comprehension
+xxs =
+  [ [1,3,5,2,3,1,2,4,5]
+  , [1,2,3,4,5,6,7,8,9]
+  , [1,2,3,4,5,6,7,8,9]
+  ]
+-- beats a double forloop huh
+filterEven = [ [ x | x <- xs, even x ] | xs <- xxs ]
+
+------------
+-- Tuples --
+------------
+{- | Tuples are like lists but they can store different types. However, the
+typing is stricter. So while you can do [[1,2], [1,2,3], [4,5]]. A Tuple
+pair is it's own type so you can't do [(1,2), (3,4,5), (5,6)]. They all
+need to be pairs or triples, but not a mix.
+
+NOTE: Tuples can have more than 2 elements. More on how to access those
+later.
+-}
+fstTuple = fst (8,11)
+sndTuple = snd (8,11)
+-- zip: combines two lists into a list of tuples
+zipExample1 = zip [1,2,3,4,5] [5,5,5,5,5]
+-- different lengths & types
+zipExample2 = zip [1,2,3,4,5] ["hello","world"]
+-- infinite list: similar to cycle
+zipExample3 = zip [1..] ["apple","orange","cherry","mango"]
