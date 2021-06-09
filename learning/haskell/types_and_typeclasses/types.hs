7f919815e72eb771bf143f235ebb8e3a22432d9b
Nikeeth <nikeeth.ramanathan@gmail.com>
2021-06-10 09:28:27 +1000

    starting on types

---
+++ learning/haskell/types_and_typeclasses/types.hs
@@ -0,0 +1,39 @@
+{-|
+Haskell has a static type system. Everything as a type - so everything is
+resolved at compile time. Makes Haskell very safe. Haskell can infer types.
+e.g.
+ghci> :t 'a'  
+'a' :: Char  
+ghci> :t True  
+True :: Bool  
+ghci> :t "HELLO!"  
+"HELLO!" :: [Char]  
+ghci> :t (True, 'a')  
+(True, 'a') :: (Bool, Char)  
+ghci> :t 4 == 5  
+4 == 5 :: Bool  
+-}
+
+-- :: is the 'has type of' operator
+-- We can explicitly define the type of the function
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
+-- Note that [Char] is the same as String so they are interchangeable
+
+-- Multiple parameters, there is no special difference between separating inputs
+-- and outputs, they all use ->
+-- Note: output is always the last parameter.
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+-- Int: signed integer but bounded (32 bits)
+
+-- Integer: signed integer but can be really long... e.g. a dumb way in  C++
+-- this will be stored in an array of integers and some extra arithmetic
+-- operations will exist behind the scenes. Whatever way haskell does it it will
+-- be slower than Int for similar reasons.
+factorial :: Integer -> Integer  
+factorial n = product [1..n]
+reallyLongInteger = factorial 50
+-- Note: Int -> Integer doesn't work even if the input can be an Int this is
+-- probably because some arithmetic operations will preserve type.
