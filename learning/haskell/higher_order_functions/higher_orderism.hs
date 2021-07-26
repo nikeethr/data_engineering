a3e523a348fa25353ae9fdbc622efffb13e358e6
Nikeeth <nikeeth.ramanathan@gmail.com>
2021-07-26 15:39:31 +1000

    maps and filters

--- learning/haskell/higher_order_functions/higher_orderism.hs
+++ learning/haskell/higher_order_functions/higher_orderism.hs
@@ -20,3 +20,73 @@ applyTwice f x = f (f x)
 sixPlusTen = applyTwice (+3) 10
 prependThreeTwice = applyTwice (3:) [1]
 hahaHahaHey = applyTwice ("HAHA " ++) "HEY"
+
+{-
+    Example: our implementation of zip with.
+    This function is really useful in that it combines two lists by applying
+    a function on each pair of elements.
+    My implementation:
+-}
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' f xs ys =  [ f x y | (x, y) <- zip' xs ys ]
+    where zip' :: [a] -> [b] -> [(a, b)]
+          zip' [] _ = []
+          zip' _ [] = []
+          zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+{-
+    Actual implementation:
+    - bypasses the usage of zip
+-}
+
+zipWith'' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith'' _ [] _ = []
+zipWith'' _ _ [] = []
+zipWith'' f (x:xs) (y:ys) = f x y : zipWith'' f xs ys
+
+{-
+    zipWith usage - element wise multiplication in a 2-D array ... yes...
+    - The inner zipWith creates a partial function that multiplies two 1-D
+      arrays element-wise.
+    - The outer zipWith applies the 1-D array multiplcation to the each element
+      of the 2-D arrays.
+-}
+
+elemwiseMult2D = zipWith'' (zipWith'' (*)) x y
+    where x = [ [1, 2, 3], [2, 3, 4], [1, 1, 1] ]
+          y = [ [3, 2, 1], [4, 3, 2], [1, 1, 1] ]
+
+{-
+    Example: flip - takes a function and two arguments and returns the same
+    function with the arguments flipped.
+    
+    My answer:
+    - This doesn't do anything special, it doesn't return a function since it
+      simply evaluate the function with the arguments flipped
+-}
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f x y = f y x
+
+{-
+    Actual answer:
+    - here we are defining the flipped function the where clause and returning
+      it instead of evaluating it.
+-}
+flip'' :: (a -> b -> c) -> (b -> a -> c)
+flip'' f = g
+    where g x y = f y x
+{-
+    Actual actual answer:
+    - Ok so the simplified answer is the same as my answer. I accidentally got
+      it right
+    - What I forgot was that putting in partial arguments will return a partial
+      function. So flip' func is the same as flip'' func.
+-}
+{-
+    Flip usage example 1: zip but flip elements
+    - will instead do zip "hello" [1,2,3,4,5]
+-}
+flipElements = flip' zip [1,2,3,4,5] "hello"
+{-
+    Flip usage example 2: zipWith but flip division operation on elements
+-}
+flipDiv = flip' (zipWith' div) [2,2..] [32,16,8,4,2]
---
+++ learning/haskell/higher_order_functions/maps_and_filters.hs
@@ -0,0 +1,66 @@
+{-
+    Map takes a function and a list and applies that function to each element in the list
+
+    Note that it is similar to list comprehension.
+    [ x+3 | x <- [1,5,3,1,6] ] is the same as map (+3) [1,5,3,1,6]
+
+    However, map can be more readible when it comes to e.g. maps of maps.
+-}
+map' :: (a -> b) -> [a] -> [b]
+map' _ []     = []
+map' f (x:xs) = f x : map' f xs
+
+{-
+    Filter takes a predicate (a function that tells if something is true or not) and a list.
+
+    It returns a list with all the elements that satisfies the predicate.
+-}
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ []      = []
+filter' f (x:xs)
+    | f x         = x : filter' f xs
+    | otherwise   = filter' f xs
+
+{-
+    Quicksort revisited.
+
+    Similar to map, filter also can be represented via a list comprehension.
+    Here is an alternate version of quicksort that uses filter which arguably
+    might be more readible.
+-}
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' []     = []
+quicksort' (x:xs) = smaller ++ x : larger
+    where larger  = quicksort' (filter' (>x) xs)
+          smaller = quicksort' (filter' (<=x) xs)
+
+{-
+    Maps and filters are bread and butter of a functional programmer's tool box
+    needed for solving complex problems by breaking them down.
+-}
+
+{-
+    Example: find largest number up to 100,000 divisible by 3829
+-}
+largestDivisible :: Integer -> Integer -> Integer
+largestDivisible x y = last [ j |  j <- [1..y], j `rem` x == 0 ]
+{-
+    Another way to write this, is to simply get a sequence with interval of
+    3829 up to 100,000
+-}
+largestDivisible' :: Integer -> Integer -> Integer
+largestDivisible' x y = last [x,x*2..y]
+{-
+    Actual answer: Using filter
+    - Note traversing the list backwards is better, since haskell is lazy in
+    evaluation so it will stop at the frst instance where the predicate is true
+    and hence return the largest value.
+    - Also using Integral type as that is more general (can apply to Int or
+    Integer).
+
+    I personally find the list comprehension more readible in this instance but
+    there are other examples coming up...
+-}
+largestDivisible'' :: (Integral a) => a -> a -> a
+largestDivisible'' x y = head (filter (p x) [y,y-1..1])
+    where p i j = j `mod` i == 0
